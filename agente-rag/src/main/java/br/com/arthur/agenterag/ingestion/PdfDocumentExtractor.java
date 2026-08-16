046e1fa276818755672a0eb07a9b6910b41798d9
Arthur Fernando Carvalho Ferreira <arthurcarvalhof@gmail.com>
2026-08-15 22:01:11 -0300

    first commit

---
+++ agente-rag/src/main/java/br/com/arthur/agenterag/ingestion/PdfDocumentExtractor.java
@@ -0,0 +1,36 @@
+package br.com.arthur.agenterag.ingestion;
+
+import org.apache.pdfbox.Loader;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+import org.springframework.stereotype.Component;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+@Component
+public class PdfDocumentExtractor implements DocumentExtractor {
+
+    @Override
+    public String extract(InputStream inputStream) throws IOException {
+        byte[] bytes = readAllBytes(inputStream);
+        try (PDDocument document = Loader.loadPDF(bytes)) {
+            PDFTextStripper stripper = new PDFTextStripper();
+            return stripper.getText(document);
+        }
+    }
+
+    @Override
+    public boolean supports(String fileExtension) {
+        return "pdf".equalsIgnoreCase(fileExtension);
+    }
+
+    private byte[] readAllBytes(InputStream inputStream) throws IOException {
+        try (ByteArrayOutputStream buffer = new ByteArrayOutputStream()) {
+            inputStream.transferTo(buffer);
+            return buffer.toByteArray();
+        }
+    }
+
+}
